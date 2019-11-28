e2b84f22bcc77bfe251ba973c4dbe998b0cc47a4
Lucas <lucas.dominguez.pro@gmail.com>
2019-11-28 17:29:05 +0100

    Ajout des imports pour les agents

--- KNNAgents.py
+++ KNNAgents.py
@@ -4,7 +4,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn import neighbors
 from multiAgents import MultiAgentSearchAgent
-
+from multiAgents import MultiAgentSearchAgent, extractFeature, getActionByNumber
 class KNNAgent(MultiAgentSearchAgent):
   def __init__(self):
     self.dataTrain = pd.read_csv("dataGameWonMoreThan1500WithColumnNames.csv")
