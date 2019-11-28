c5fbc5eba630e9c8cebf70939a73a345111cc4b3
Mohamed Badaoui <mohamed.badaoui13@gmail.com>
2019-11-28 16:34:13 +0100

    adding svm agent

--- multiAgents.py
+++ multiAgents.py
@@ -16,6 +16,7 @@ from MonteCarlo import MCTS, Node
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn import neighbors
+from sklearn.svm import SVC
 
 class ReflexAgent(Agent):
   """
@@ -728,12 +729,11 @@ class KNNAgent(MultiAgentSearchAgent):
     self.knn = neighbors.KNeighborsClassifier(n_neighbors=3)
     self.knn.fit(xtrain, ytrain)
     
-    
   def getAction(self, currGameState):
     data = pd.DataFrame(columns=["ghostUp","ghostDown","ghostLeft","ghostRight","wallUp","wallDown","wallLeft","wallRight","foodUp","foodDown","foodLeft","foodRight","emptyUp","emptyDown","emptyLeft","emptyRight","nearestFood","nearestGhost","nearestCapsule","legalPositionUp","legalPositionDown","legalPositionULeft","legalPositionRight","pacmanPositionX","pacmanPositionY","labelNextAction"])
     data.loc[0,:] = extractFeature(currGameState, "South")
     data.drop(columns=["labelNextAction"], axis=1)
-    nextActionNumber = self.knn.predict(data)
+    nextActionNumber = self.svclassifier.predict(data)
     
     nextPredictedAction = getActionByNumber(nextActionNumber)
     
@@ -743,6 +743,36 @@ class KNNAgent(MultiAgentSearchAgent):
       nextPredictedAction  = currGameState.getLegalActions(0)[random.randrange(0, len(currGameState.getLegalActions(0)))]
     
     return nextPredictedAction
+
+class SVMAgent(MultiAgentSearchAgent):
+  def __init__(self):
+    self.dataTrain = pd.read_csv("dataGameWonMoreThan1500WithColumnNames.csv")
+    self.dataTarget = self.dataTrain["labelNextAction"]
+    self.dataTrain = self.dataTrain.drop(columns=["labelNextAction"], axis=1)
+    xtrain, xtest, ytrain, ytest = train_test_split(self.dataTrain, self.dataTarget, train_size=0.8)
+    self.svclassifier = SVC(kernel='rbf')
+    self.svclassifier.fit(xtrain, ytrain)
+
+  def getAction(self, currGameState):
+    data = pd.DataFrame(
+      columns=["ghostUp", "ghostDown", "ghostLeft", "ghostRight", "wallUp", "wallDown", "wallLeft", "wallRight",
+                "foodUp", "foodDown", "foodLeft", "foodRight", "emptyUp", "emptyDown", "emptyLeft", "emptyRight",
+                "nearestFood", "nearestGhost", "nearestCapsule", "legalPositionUp", "legalPositionDown",
+                "legalPositionULeft", "legalPositionRight", "pacmanPositionX", "pacmanPositionY", "labelNextAction"])
+    data.loc[0, :] = extractFeature(currGameState, "South")
+    data.drop(columns=["labelNextAction"], axis=1)
+    nextActionNumber = self.svclassifier.predict(data)
+
+    nextPredictedAction = getActionByNumber(nextActionNumber)
+
+    if (nextPredictedAction not in currGameState.getLegalActions(0)):
+      print("Illegal Action")
+      nextPredictedAction = currGameState.getLegalActions(0)[
+        random.randrange(0, len(currGameState.getLegalActions(0)))]
+
+    return nextPredictedAction
+
+
 
 class MonteCarloAgent(MultiAgentSearchAgent):
   """
