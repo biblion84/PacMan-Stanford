b500877b9133503da2631aca8a160dcc5e864634
Unknown <oblipn@gmail.com>
2019-11-29 02:06:42 +0100

    test keras

--- multiAgents.py
+++ multiAgents.py
@@ -18,6 +18,10 @@ from sklearn.model_selection import train_test_split
 from sklearn import neighbors
 from sklearn.svm import SVC
 from sklearn.linear_model import Ridge
+import keras
+from keras import losses
+from keras.models import Sequential
+from keras.layers import Dense
 
 class ReflexAgent(Agent):
   """
@@ -721,27 +725,42 @@ def extractFeature(gameState, actionChoosed):
   return dataFrameCurrentState
 #TODO Nombre de bouffe totale en haut, gauche, droite
 
-class KNNAgent(MultiAgentSearchAgent):
+class KerasAgent(MultiAgentSearchAgent):
+  model = None
   def __init__(self):
+    model = Sequential()
+    model.add(Dense(units=128, activation='softmax', input_dim=25))
+    model.add(Dense(units=128, activation='relu'))
+    # model.add(Dense(units=128, activation='relu'))
+    # model.add(Dense(units=128, activation='relu'))
+    model.add(Dense(units=1, activation='relu'))
+    # model.summary()
+    model.compile(loss=losses.mean_squared_error,
+                  optimizer='sgd',
+                  metrics=['accuracy'])
+    model.compile(loss=keras.losses.mean_squared_error,
+                  optimizer=keras.optimizers.SGD(lr=0.01, momentum=0.9, nesterov=True))
     self.dataTrain = pd.read_csv("dataGameWonMoreThan1500WithColumnNames.csv")
     self.dataTrain = self.dataTrain.drop(self.dataTrain.columns[0], axis=1)
     self.dataTarget = self.dataTrain["labelNextAction"]
     self.dataTrain = self.dataTrain.drop(columns=["labelNextAction"], axis=1)
-    xtrain, xtest, ytrain, ytest = train_test_split(self.dataTrain, self.dataTarget, train_size=0.8)
-    self.knn = neighbors.KNeighborsClassifier(n_neighbors=1)
-    self.knn.fit(xtrain, ytrain)
+    model.fit(self.dataTrain, self.dataTarget, epochs=5, batch_size=128)
+    loss_and_metrics = model.evaluate(self.dataTrain, self.dataTarget)
+    self.model = model
+    print(loss_and_metrics)
     
   def getAction(self, currGameState):
     data = pd.DataFrame(columns=["ghostUp","ghostDown","ghostLeft","ghostRight","wallUp","wallDown","wallLeft","wallRight","foodUp","foodDown","foodLeft","foodRight","emptyUp","emptyDown","emptyLeft","emptyRight","nearestFood","nearestGhost","nearestCapsule","legalPositionUp","legalPositionDown","legalPositionULeft","legalPositionRight","pacmanPositionX","pacmanPositionY","labelNextAction"])
     data.loc[0,:] = extractFeature(currGameState, "South")
     dataTrain = data.drop(columns=["labelNextAction"], axis=1)
-    nextActionNumber = self.knn.predict(dataTrain)
-    
+    nextActionNumber = self.model.predict(dataTrain)
+    print (nextActionNumber)
     nextPredictedAction = getActionByNumber(nextActionNumber)
     
     
     if (nextPredictedAction not in currGameState.getLegalActions(0) ):
       print("Illegal Action")
+      print(nextPredictedAction)
       nextPredictedAction  = currGameState.getLegalActions(0)[random.randrange(0, len(currGameState.getLegalActions(0)))]
     
     return nextPredictedAction
