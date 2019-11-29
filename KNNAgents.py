60df1951e497434fb0e39610c63879bd5e08390e
Lucas <lucas.dominguez.pro@gmail.com>
2019-11-29 11:26:40 +0100

    Ajout de l'entrainement et de la sauvegarde avec joblib

--- KNNAgents.py
+++ KNNAgents.py
@@ -1,15 +1,11 @@
 from multiAgents import *
 from sklearn.model_selection import train_test_split
 from sklearn import neighbors
+from joblib import dump, load
 
 class KNNAgent(MultiAgentSearchAgent):
   def __init__(self):
-    self.dataTrain = pd.read_csv("dataGameWonMoreThan1500WithColumnNames.csv")
-    self.dataTarget = self.dataTrain["labelNextAction"]
-    self.dataTrain = self.dataTrain.drop(columns=["labelNextAction"], axis=1)
-    xtrain, xtest, ytrain, ytest = train_test_split(self.dataTrain, self.dataTarget, train_size=0.8)
-    self.knn = neighbors.KNeighborsClassifier(n_neighbors=1)
-    self.knn.fit(xtrain, ytrain)
+    self.knn = load('TrainedModels/knn.joblib')
   
   def getAction(self, currGameState):
     data = pd.DataFrame(
@@ -23,5 +19,4 @@ class KNNAgent(MultiAgentSearchAgent):
     if (nextPredictedAction not in currGameState.getLegalActions(0)):
       print("Illegal Action")
       nextPredictedAction = currGameState.getLegalActions(0)[random.randrange(0, len(currGameState.getLegalActions(0)))]
-    
-    return nextPredictedAction
+    return  nextPredictedAction
---
+++ TrainedModels/knnTrain.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn import neighbors
+from joblib import dump, load
+
+
+data = pd.read_csv("../dataGameWonMoreThan1500WithColumnNames.csv")
+y = data["labelNextAction"]
+X = data.drop(columns=["labelNextAction"], axis=1)
+xtrain, xtest, ytrain, ytest = train_test_split(X, y, train_size=0.8)
+knn = neighbors.KNeighborsClassifier(n_neighbors=1)
+knn.fit(xtrain, ytrain)
+
+dump(knn, 'knn.joblib', protocol=2)
